a2c4f5de16000b3309f424200f4ff15ceb322e54
Racknar0 <racknarow1@gmail.com>
2022-04-17 13:28:40 -0500

    Adñadiendo un listener

--- js/script.js
+++ js/script.js
@@ -12,23 +12,38 @@ function preguntar() {
 
     usuario.name = preguntaAlUser('el nombre del usuario').toLowerCase();
     usuario.lastName = preguntaAlUser('el apellido del usuario').toLowerCase();
-     
+
     //!Verificar que el telefono sea valido1
     usuario.phoneNumber = parseInt(preguntaAlUser('el telefono del usuario'));
-    while (isNaN(usuario.phoneNumber) || usuario.phoneNumber < 1 || usuario.phoneNumber.toString().length !== 10) {
-        alert('Ingrese un valor Numérico Valido y con una extension nacional de 10 digitos');
-        usuario.phoneNumber = parseInt(preguntaAlUser('el telefono del usuario'));
+    while (
+        isNaN(usuario.phoneNumber) ||
+        usuario.phoneNumber < 1 ||
+        usuario.phoneNumber.toString().length !== 10
+    ) {
+        alert(
+            'Ingrese un valor Numérico Valido y con una extension nacional de 10 digitos'
+        );
+        usuario.phoneNumber = parseInt(
+            preguntaAlUser('el telefono del usuario')
+        );
     }
 
-    usuario.email = preguntaAlUser('Ingrese el correo del usuario').toLowerCase();
+    usuario.email = preguntaAlUser(
+        'Ingrese el correo del usuario'
+    ).toLowerCase();
     usuario.brand = preguntaAlUser('Ingrese la marca del equipo').toLowerCase();
-    usuario.model = preguntaAlUser('Ingrese el modelo del equipo').toLowerCase(); 
+    usuario.model = preguntaAlUser(
+        'Ingrese el modelo del equipo'
+    ).toLowerCase();
 
     //!Validar Cantidad de Fallos
     const arrFallos = cantidadFallos();
-    usuario.fallos = arrFallos 
+    usuario.fallos = arrFallos;
 
-    usuario.diagnostic = preguntaAlUser('Ingrese el diagnostico del equipo', '').toLowerCase();  
+    usuario.diagnostic = preguntaAlUser(
+        'Ingrese el diagnostico del equipo',
+        ''
+    ).toLowerCase();
 
     return usuario;
 }
@@ -101,6 +116,57 @@ function filtroBuscarUsuario(usuarios, cantidad) {
     alert('El usuario no existe');
 }
 
+
+function mostrarResultado(usuarios) {
+    /************** ( 'DOM SCRIPTING' ) **************/
+    divResultadoTitulo = document.createElement('h1');
+    divResultadoTitulo.textContent = `Cantidad de usuarios registrados: ${usuarios.length}`;
+    divResultado.appendChild(divResultadoTitulo);
+
+    for (let i = 0; i < usuarios.length; i++) {
+        const divNumeroDelUsuario = document.createElement('h2');
+        divNumeroDelUsuario.textContent = `Usuario Numero ${i + 1}`;
+        divResultado.appendChild(divNumeroDelUsuario);
+
+        const divUsuarioNombre = document.createElement('p');
+        divUsuarioNombre.textContent = `Nombre: ${usuarios[i].name}`;
+        divResultado.appendChild(divUsuarioNombre);
+
+        const divUsuarioApellido = document.createElement('p');
+        divUsuarioApellido.textContent = `Apellido: ${usuarios[i].lastName}`;
+        divResultado.appendChild(divUsuarioApellido);
+
+        const divUsuarioTelefono = document.createElement('p');
+        divUsuarioTelefono.textContent = `Telefono: ${usuarios[i].phoneNumber}`;
+        divResultado.appendChild(divUsuarioTelefono);
+
+        const divUsuarioCorreo = document.createElement('p');
+        divUsuarioCorreo.textContent = `Correo: ${usuarios[i].email}`;
+        divResultado.appendChild(divUsuarioCorreo);
+
+        const divUsuarioMarca = document.createElement('p');
+        divUsuarioMarca.textContent = `Marca: ${usuarios[i].brand}`;
+        divResultado.appendChild(divUsuarioMarca);
+
+        const divUsuarioModelo = document.createElement('p');
+        divUsuarioModelo.textContent = `Modelo: ${usuarios[i].model}`;
+        divResultado.appendChild(divUsuarioModelo);
+
+        const divUsuarioFallos = document.createElement('p');
+        divUsuarioFallos.textContent = `Cantidad de fallos: ${usuarios[i].fallos.length}`;
+        divResultado.appendChild(divUsuarioFallos);
+
+        const divUsuarioDiagnostico = document.createElement('p');
+        divUsuarioDiagnostico.textContent = `Diagnostico: ${usuarios[i].diagnostic}`;
+        divResultado.appendChild(divUsuarioDiagnostico);
+
+        const divUsuarioFecha = document.createElement('p');
+        divUsuarioFecha.textContent = `Fecha:AUN NO CREADA`;
+        divResultado.appendChild(divUsuarioFecha);
+    }
+}
+
+
 /************** ( 'CUERPO DEL CODIGO' ) **************/
 
 cantidad = parseInt(prompt('Ingrese la cantidad de usuarios a registrar', 0));
@@ -137,52 +203,14 @@ switch (filtro) {
         break;
 }
 
+const mostrar = document.querySelector('#mostrar');
+mostrar.addEventListener('click', () => {
+    mostrarResultado(usuarios);
+});
+
+
 console.log(usuarios);
 
-/************** ( 'DOM SCRIPTING' ) **************/
-divResultadoTitulo = document.createElement('h1');
-divResultadoTitulo.textContent = `Cantidad de usuarios registrados: ${usuarios.length}`;
-divResultado.appendChild(divResultadoTitulo);
 
-for (let i = 0; i < usuarios.length; i++) {
 
-    const divNumeroDelUsuario = document.createElement('h2');
-    divNumeroDelUsuario.textContent = `Usuario Numero ${i + 1}`;
-    divResultado.appendChild(divNumeroDelUsuario);
 
-    const divUsuarioNombre = document.createElement('p');
-    divUsuarioNombre.textContent = `Nombre: ${usuarios[i].name}`;
-    divResultado.appendChild(divUsuarioNombre);
-
-    const divUsuarioApellido = document.createElement('p');
-    divUsuarioApellido.textContent = `Apellido: ${usuarios[i].lastName}`;
-    divResultado.appendChild(divUsuarioApellido);
-
-    const divUsuarioTelefono = document.createElement('p');
-    divUsuarioTelefono.textContent = `Telefono: ${usuarios[i].phoneNumber}`;
-    divResultado.appendChild(divUsuarioTelefono);
-
-    const divUsuarioCorreo = document.createElement('p');
-    divUsuarioCorreo.textContent = `Correo: ${usuarios[i].email}`;
-    divResultado.appendChild(divUsuarioCorreo);
-
-    const divUsuarioMarca = document.createElement('p');
-    divUsuarioMarca.textContent = `Marca: ${usuarios[i].brand}`;
-    divResultado.appendChild(divUsuarioMarca);
-
-    const divUsuarioModelo = document.createElement('p');
-    divUsuarioModelo.textContent = `Modelo: ${usuarios[i].model}`;
-    divResultado.appendChild(divUsuarioModelo); 
-
-    const divUsuarioFallos = document.createElement('p');
-    divUsuarioFallos.textContent = `Cantidad de fallos: ${usuarios[i].fallos.length}`;
-    divResultado.appendChild(divUsuarioFallos);
-
-    const divUsuarioDiagnostico = document.createElement('p');
-    divUsuarioDiagnostico.textContent = `Diagnostico: ${usuarios[i].diagnostic}`;
-    divResultado.appendChild(divUsuarioDiagnostico);
-
-    const divUsuarioFecha = document.createElement('p');
-    divUsuarioFecha.textContent = `Fecha:AUN NO CREADA`;
-    divResultado.appendChild(divUsuarioFecha);
-}
